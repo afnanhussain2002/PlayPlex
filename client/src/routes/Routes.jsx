e785cc425f7ba2e06e6affa8f1551d79f2b90c59
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-06-10 14:30:46 +0600

    Client: Add some Routes

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -1,12 +1,25 @@
 
 import { createBrowserRouter } from "react-router-dom"
 import Root from "../root/Root"
+import Home from "../pages/home/Home"
+import About from "../pages/about/About"
 
 const router = createBrowserRouter([
          {
             path:'/',
-            element:<Root/>
-         }
+            element:<Root/>,
+            children:[
+               {
+                  path:'/',
+                  element:<Home/>
+               },
+               {
+                  path:'/about',
+                  element:<About/>
+               }
+            ]
+         },
+         
 ])
 
 export default router
