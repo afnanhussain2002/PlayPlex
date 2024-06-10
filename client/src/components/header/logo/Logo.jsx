b0e2a2414537985ab8c66cb9af4f358a3073191f
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-06-10 15:06:10 +0600

    Client: Add logo and make some fake routes

---
+++ client/src/components/header/logo/Logo.jsx
@@ -0,0 +1,11 @@
+
+
+const Logo = () => {
+    return (
+        <div>
+            <img className="w-40" src="https://i.ibb.co/qMxM6vQ/logo.png" alt="logo" />
+        </div>
+    );
+};
+
+export default Logo;
--- client/src/root/Root.jsx
+++ client/src/root/Root.jsx
@@ -1,9 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Header from "../components/header/header/Header";
+import Footer from "../components/footer/Footer";
 
 const Root = () => {
     return (
-        <div>
-            This is The main folder
-        </div>
+        <>
+            <Header/>
+            <Outlet/>
+            <Footer/>
+        </>
     );
 };
 
