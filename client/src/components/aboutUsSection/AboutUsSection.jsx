05c1062345dc08d36edca04869c9c3e3ab768e78
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-06-12 06:08:31 +0600

    Client: Add top rated user images

--- client/src/components/aboutUsSection/AboutUsSection.jsx
+++ client/src/components/aboutUsSection/AboutUsSection.jsx
@@ -12,6 +12,16 @@ const AboutUsSection = () => {
             <button className="btn-md border-2 border-main-color text-main-color rounded font-bold mr-2">Buy Hero </button>
             <button className="btn-md bg-main-color text-black rounded font-bold shadow-lg shadow-green-500/50">Buy Nfts </button>
         </div>
+        <hr className="bg-main-color w-96 my-4 mx-auto h-1"/>
+        <div className="flex justify-center gap-4">
+          <img src="https://themedox.com/mykd/wp-content/uploads/2023/10/about_tab01.png" alt="" />
+          <img src="https://themedox.com/mykd/wp-content/uploads/2023/10/about_tab02.png" alt="" />
+          <img src="https://themedox.com/mykd/wp-content/uploads/2023/10/about_tab03.png" alt="" />
+          <img src="https://themedox.com/mykd/wp-content/uploads/2023/10/about_tab04.png" alt="" />
+          <img src="https://themedox.com/mykd/wp-content/uploads/2023/10/about_tab05.png" alt="" />
+          <img src="https://themedox.com/mykd/wp-content/uploads/2023/10/about_tab06.png" alt="" />
+        </div>
+        <hr className="bg-main-color w-96 my-4 mx-auto h-1"/>
       </div>
     </div>
   );
