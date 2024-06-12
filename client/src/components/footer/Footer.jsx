f1449423bacbdbffc58d7eb980d65926ac7feb5c
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-06-12 08:18:35 +0600

    Client: Add Footer section

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,40 @@
+import Logo from'../../components/header/logo/Logo'
+const Footer = () => {
+  return (
+    <div className=' bg-base-200'>
+      <footer className="footer p-10 text-base-content max-w-7xl mx-auto">
+        <aside>
+         <Logo/>
+          <p>
+            ACME Industries Ltd.
+            <br />
+            Providing reliable tech since 1992
+          </p>
+        </aside>
+        <nav>
+          <h6 className="footer-title">Services</h6>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Company</h6>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Legal</h6>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </nav>
+       
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
