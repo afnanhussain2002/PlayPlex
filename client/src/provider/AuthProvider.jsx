5911772176f8cbf21bc16c05a622fc6265bb4f05
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-06-13 21:51:35 +0600

    Client: create user, login user, google sign add on context page

--- client/src/provider/AuthProvider.jsx
+++ client/src/provider/AuthProvider.jsx
@@ -1,15 +1,33 @@
 import { createContext } from "react";
 import auth from "../firebase/firebase.config";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 
 const AuthContext = createContext(null)
+const googleProvider = new GoogleAuthProvider()
 
 
-const AuthProvider = () => {
+const AuthProvider = ({children}) => {
 
-    const authInfo = {}
+    // create a user 
+    const createUser = (email,password) =>{
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    // login user
+    const loginUser = (email, password) =>{
+        return signInWithEmailAndPassword(email, password)
+    }
+
+    // sign user with google
+    const google = () =>{
+        return signInWithPopup(googleProvider)
+    }
+
+
+    const authInfo = {createUser, loginUser, google}
     return (
         <AuthContext.Provider value={authInfo}>
-            
+           {children} 
         </AuthContext.Provider>
     );
 };
