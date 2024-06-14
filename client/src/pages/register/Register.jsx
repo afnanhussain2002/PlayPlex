b597d28b6c2f9e7a5fa82230e4627c02cadc5643
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-06-14 11:14:02 +0600

    Client: set a state for track the user and set a loading state

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,7 +1,7 @@
 import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../provider/AuthProvider";
-import { updateProfile } from "firebase/auth";
+
 
 
 const Register = () => {
--- client/src/provider/AuthProvider.jsx
+++ client/src/provider/AuthProvider.jsx
@@ -1,6 +1,6 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import auth from "../firebase/firebase.config";
-import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 
 export const AuthContext = createContext(null)
 const googleProvider = new GoogleAuthProvider()
@@ -8,23 +8,51 @@ const googleProvider = new GoogleAuthProvider()
 
 const AuthProvider = ({children}) => {
 
+    const [user, setUser] = useState()
+    const [loading, setLoading] = useState(true)
+
     // create a user 
     const createUser = (email,password) =>{
+        setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
     // login user
     const loginUser = (email, password) =>{
+        setLoading(true)
         return signInWithEmailAndPassword(auth,email, password)
     }
 
     // sign user with google
     const google = () =>{
+        setLoading(true)
         return signInWithPopup(googleProvider)
     }
 
+    // logout user 
+    const logout = () =>{
+        setLoading(true)
+        return signOut(auth)
+    }
 
-    const authInfo = {createUser, loginUser, google}
+    // Track the user
+
+    useEffect(() =>{
+       const unSubscribe = onAuthStateChanged( auth, currentUser =>{
+            setUser(currentUser)
+            console.log(currentUser);
+            setLoading(false)
+        })
+
+        return () =>{
+            unSubscribe()
+        }
+
+
+    },[])
+
+
+    const authInfo = {user, loading, createUser, loginUser, google, logout}
     return (
         <AuthContext.Provider value={authInfo}>
            {children} 
