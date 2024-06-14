080af2f182b48fc9dad1ff232c48527741f2716a
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-06-14 17:44:29 +0600

    Client: Change the checkbox

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -4,103 +4,152 @@ import { AuthContext } from "../../provider/AuthProvider";
 import { updateProfile } from "firebase/auth";
 
 const Register = () => {
-  const {createUser, user} = useContext(AuthContext)
-  const [regError, setRegError] = useState('')
-  const [success, setSuccess ] = useState('')
+  const { createUser, user } = useContext(AuthContext);
+  const [regError, setRegError] = useState("");
+  const [success, setSuccess] = useState("");
 
-  const handleRegister = e =>{
-    e.preventDefault()
+  const handleRegister = (e) => {
+    e.preventDefault();
     const form = e.target;
     const name = form.name.value;
     const photo = form.photo.value;
     const email = form.email.value;
     const password = form.password.value;
-    const terms = form.terms.checked
+    const terms = form.terms.checked;
 
-    const userDetails ={name, photo, email, password,terms}
+    const userDetails = { name, photo, email, password, terms };
     console.log(userDetails);
 
-    setRegError('')
-    setSuccess('')
+    setRegError("");
+    setSuccess("");
 
-    if(password.length < 8){
-     return setRegError('Password should 8 character or more')
+    if (password.length < 8) {
+      return setRegError("Password should 8 character or more");
     }
 
-    createUser(email,password)
-    .then(res =>{
-      console.log(res.user);
-      setSuccess('User Created Successfully')
-      updateProfile(user, {
-        displayName:name, photoURL:photo
+    createUser(email, password)
+      .then((res) => {
+        console.log(res.user);
+        setSuccess("User Created Successfully");
+        updateProfile(user, {
+          displayName: name,
+          photoURL: photo,
+        })
+          .then(() => {
+            console.log("profile updated");
+          })
+          .catch((error) => {
+            setRegError(error.message);
+          });
       })
-      .then(() =>{
-        console.log('profile updated');
-      })
-      .catch(error =>{
-        setRegError(error.message)
-      })
-    }) 
-    .catch(error => {
-      setRegError(error.message);
-    })
-  }
-    return (
-        <div>
-              <div className="container flex flex-col mx-auto rounded-lg pt-12 my-5">
-           <div className="flex justify-center w-full h-full my-auto xl:gap-14 lg:justify-normal md:gap-5 draggable">
-      <div className="flex items-center justify-center w-full lg:p-12">
-        <div className="flex items-center xl:p-10">
-          <form onSubmit={handleRegister} className="flex flex-col w-full h-full p-6 text-center rounded-3xl border-4 border-main-color shadow-lg shadow-green-500/100">
-            <h3 className="mb-3 text-4xl font-extrabold text-white">Register</h3>
-         
-            <button className="flex items-center border-4 border-main-color shadow-lg shadow-green-500/50 justify-center w-full py-4 mb-6 text-sm font-medium transition duration-300 rounded-2xl text-grey-900 bg-grey-300 hover:bg-grey-400 focus:ring-4 focus:ring-grey-300">
-              <img className="h-5 mr-2" src="https://raw.githubusercontent.com/Loopple/loopple-public-assets/main/motion-tailwind/img/logos/logo-google.png" alt=""/>
-              Sign in with Google
-            </button>
-            <div className="flex items-center mb-3">
-              <hr className="h-0 border-b border-solid border-grey-500 grow"/>
-              <p className="mx-4 text-grey-600">or</p>
-              <hr className="h-0 border-b border-solid border-grey-500 grow"/>
-           </div>
-          <div className="flex gap-3">
-           
-            <input id="name" type="text" name="name" placeholder="Your Name" className="flex items-center w-full px-5 py-4 mr-2 text-sm font-medium outline-none focus:bg-grey-400 mb-7 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl" required/>
+      .catch((error) => {
+        setRegError(error.message);
+      });
+  };
+  return (
+    <div>
+      <div className="container flex flex-col mx-auto rounded-lg pt-12 my-5">
+        <div className="flex justify-center w-full h-full my-auto xl:gap-14 lg:justify-normal md:gap-5 draggable">
+          <div className="flex items-center justify-center w-full lg:p-12">
+            <div className="flex items-center xl:p-10">
+              <form
+                onSubmit={handleRegister}
+                className="flex flex-col w-full h-full p-6 text-center rounded-3xl border-4 border-main-color shadow-lg shadow-green-500/100"
+              >
+                <h3 className="mb-3 text-4xl font-extrabold text-white">
+                  Register
+                </h3>
 
-            <input id="name" type="text" name="photo" placeholder="Photo" className="flex items-center w-full px-5 py-4 mr-2 text-sm font-medium outline-none focus:bg-grey-400 mb-7 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl" required/>
-
-          </div>
- 
-            <input id="email" type="email" name="email" placeholder="mail@loopple.com" className="flex items-center w-full px-5 py-4 mr-2 text-sm font-medium outline-none focus:bg-grey-400 mb-7 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl" required/>
-
-          
-            <input id="password" type="password" name="password" placeholder="Enter a password" className="flex items-center w-full px-5 py-4 mb-5 mr-2 text-sm font-medium outline-none focus:bg-grey-400 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl" required/>
-            <div className="flex flex-row justify-between mb-8">
-              <label className="relative inline-flex items-center mr-3 cursor-pointer select-none">
-                <input type="checkbox" name="terms" className="sr-only peer"/>
-                <div
-                  className="w-5 h-5 bg-white border-2 rounded-sm border-grey-500 peer peer-checked:border-0 peer-checked:bg-purple-blue-500">
-                  <img className="" src="https://raw.githubusercontent.com/Loopple/loopple-public-assets/main/motion-tailwind/img/icons/check.png" alt="tick"/>
+                <button className="flex items-center border-4 border-main-color shadow-lg shadow-green-500/50 justify-center w-full py-4 mb-6 text-sm font-medium transition duration-300 rounded-2xl text-grey-900 bg-grey-300 hover:bg-grey-400 focus:ring-4 focus:ring-grey-300">
+                  <img
+                    className="h-5 mr-2"
+                    src="https://raw.githubusercontent.com/Loopple/loopple-public-assets/main/motion-tailwind/img/logos/logo-google.png"
+                    alt=""
+                  />
+                  Sign in with Google
+                </button>
+                <div className="flex items-center mb-3">
+                  <hr className="h-0 border-b border-solid border-grey-500 grow" />
+                  <p className="mx-4 text-grey-600">or</p>
+                  <hr className="h-0 border-b border-solid border-grey-500 grow" />
                 </div>
-                <span className="ml-3 text-sm font-normal text-grey-900">Accept Our Terms and Conditions</span>
-              </label>
-              <a href="javascript:void(0)" className="mr-4 text-sm font-medium text-purple-blue-500">Forget password?</a>
+                <div className="flex gap-3">
+                  <input
+                    id="name"
+                    type="text"
+                    name="name"
+                    placeholder="Your Name"
+                    className="flex items-center w-full px-5 py-4 mr-2 text-sm font-medium outline-none focus:bg-grey-400 mb-7 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl"
+                    required
+                  />
+
+                  <input
+                    id="name"
+                    type="text"
+                    name="photo"
+                    placeholder="Photo"
+                    className="flex items-center w-full px-5 py-4 mr-2 text-sm font-medium outline-none focus:bg-grey-400 mb-7 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl"
+                    required
+                  />
+                </div>
+
+                <input
+                  id="email"
+                  type="email"
+                  name="email"
+                  placeholder="mail@loopple.com"
+                  className="flex items-center w-full px-5 py-4 mr-2 text-sm font-medium outline-none focus:bg-grey-400 mb-7 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl"
+                  required
+                />
+
+                <input
+                  id="password"
+                  type="password"
+                  name="password"
+                  placeholder="Enter a password"
+                  className="flex items-center w-full px-5 py-4 mb-5 mr-2 text-sm font-medium outline-none focus:bg-grey-400 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl"
+                  required
+                />
+                <div className="flex flex-row justify-between mb-8">
+                  <div className="form-control">
+                    <label className="label cursor-pointer">
+                      <span className="label-text">Terms & Conditions</span>
+                      <input
+                        type="checkbox"
+                        defaultChecked
+                        name="terms"
+                        className="checkbox ml-2"
+                      />
+                    </label>
+                  </div>
+                  <a
+                    href="javascript:void(0)"
+                    className="mr-4 text-sm font-medium text-purple-blue-500"
+                  >
+                    Forget password?
+                  </a>
+                </div>
+                <button className="w-full px-6 py-5 mb-5 text-sm font-bold bg-main-color rounded-lg text-black shadow-lg shadow-green-500/50">
+                  Register
+                </button>
+                {regError ? (
+                  <p className="text-red-600">{regError}</p>
+                ) : (
+                  <p className="text-green-600">{success}</p>
+                )}
+                <p className="text-sm leading-relaxed text-grey-900">
+                  Already have an Account?{" "}
+                  <Link to={"/login"} className="font-bold text-grey-700">
+                    Login your Account
+                  </Link>
+                </p>
+              </form>
             </div>
-            <button className="w-full px-6 py-5 mb-5 text-sm font-bold bg-main-color rounded-lg text-black shadow-lg shadow-green-500/50">Register</button>
-            {
-              regError ? <p className="text-red-600">{regError}</p>
-              :
-              <p className="text-green-600">{success}</p>
-            }
-            <p className="text-sm leading-relaxed text-grey-900">Already have an Account? <Link to={'/login'} className="font-bold text-grey-700">Login your Account</Link></p>
-          </form>
+          </div>
         </div>
       </div>
     </div>
-        </div>
-       
-        </div>
-    );
+  );
 };
 
 export default Register;
