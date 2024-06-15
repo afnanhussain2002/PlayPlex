33646cd20cc0849c17e8d5fe7f923d8fb2936a7b
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-06-15 18:06:30 +0600

    Client: profile picture add after login, and add logout button

--- client/src/components/header/header/Header.jsx
+++ client/src/components/header/header/Header.jsx
@@ -2,8 +2,12 @@ import { NavLink } from "react-router-dom";
 import Logo from "../logo/Logo";
 import HeaderBtn from "../../buttons/headerBtn/HeaderBtn";
 import { FaRegEdit } from "react-icons/fa";
+import { useContext } from "react";
+import { AuthContext } from "../../../provider/AuthProvider";
+import Swal from 'sweetalert2'
 
 const Header = () => {
+  const { user, logout } = useContext(AuthContext);
   const menu = (
     <>
       <li className="font-bold">
@@ -13,7 +17,7 @@ const Header = () => {
             isPending
               ? "pending"
               : isActive
-              ? "text-main-color line-through"
+              ? "text-main-color "
               : ""
           }
         >
@@ -52,44 +56,95 @@ const Header = () => {
       </li>
     </>
   );
+  // handle logout
+  const handleLogout = () => {
+    logout()
+      .then(() => {
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Log out Successfully",
+          showConfirmButton: false,
+          timer: 1500
+        });
+      })
+      .catch();
+  };
   return (
     <div className="flex justify-center">
-    <div className="navbar absolute z-40 max-w-7xl">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+      <div className="navbar absolute z-40 max-w-7xl">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
+              {menu}
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52"
-          >
-            {menu}
-          </ul>
+          <Logo />
         </div>
-        <Logo />
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{menu}</ul>
+        </div>
+        <div className="navbar-end">
+          {user ? (
+            <div className="dropdown dropdown-end">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-circle avatar border-4 border-main-color"
+              >
+                <div className="w-10 rounded-full">
+                  <img
+                    alt={user.displayName}
+                    src={user.photoURL}
+                  />
+                </div>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a className="justify-between">
+                    Profile
+                    <span className="badge">New</span>
+                  </a>
+                </li>
+                <li>
+                  <a>Settings</a>
+                </li>
+                <li>
+                  <a onClick={handleLogout}>Logout</a>
+                </li>
+              </ul>
+            </div>
+          ) : (
+            <HeaderBtn
+              link={"/register"}
+              text={"-Sign In"}
+              icon={<FaRegEdit />}
+            />
+          )}
+        </div>
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">{menu}</ul>
-      </div>
-      <div className="navbar-end">
-        <HeaderBtn link={'/register'} text={'-Sign In'} icon={<FaRegEdit/>}/>
-      </div>
-    </div>
-
     </div>
   );
 };
