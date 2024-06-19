8d0de3709616530368db6fd4fe5bb00c63dbeb10
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-06-19 21:02:18 +0600

    Client: Add daynamic data and remove fake data

--- client/src/components/products/Products.jsx
+++ client/src/components/products/Products.jsx
@@ -5,7 +5,7 @@ const Products = () => {
     const [products, setProducts] = useState([])
 
     useEffect(() =>{
-        fetch('./data/product.json')
+        fetch('http://localhost:5000/games')
         .then(res => res.json())
         .then(data => setProducts(data))
     })
--- client/src/components/singleProduct/SingleProduct.jsx
+++ client/src/components/singleProduct/SingleProduct.jsx
@@ -4,25 +4,25 @@ import { Link } from "react-router-dom";
 
 const SingleProduct = ({product}) => {
     
-    const { id, creator_img, creator_name, price, product_img, product_name } = product
+    const { _id, productName, productPrice, productPhoto, uploaderName, uploaderPhoto} = product
     return (
         <>
            <div className="bg-sec-color border border-main-color flex flex-col justify-center gap-5 items-center rounded-lg py-5 
             shadow-lg shadow-green-500/50 lg:flex-row">
             <div>
-              <img className="w-28 h-20 rounded-lg" src={product_img} alt={product_name} />
+              <img className="w-28 h-20 rounded-lg" src={productPhoto} alt={productName} />
             </div>
             <div>
-              <h3 className="text-2xl font-bold text-white">{product_name}</h3>
+              <h3 className="text-2xl font-bold text-white">{productName}</h3>
               <div className="flex items-center gap-1 text-xs">
-                <img className="w-10" src={creator_img} alt="creator" />
-                <p>{creator_name} ||</p>
+                <img className="w-8 rounded-full" src={uploaderPhoto} alt="creator" />
+                <p>{uploaderName} ||</p>
                 <p>Creator</p>
               </div>
               <div className="flex items-center gap-5 justify-around border p-2 rounded border-main-color">
-                <p className="font-bold">${price}</p>
+                <p className="font-bold">${productPrice}</p>
                 <div>
-                 <Link to={`/product/${id}`}><button className=" btn-sm bg-main-color text-black rounded">Buy Now</button></Link> 
+                 <Link to={`/product/${_id}`}><button className=" btn-sm bg-main-color text-black rounded">Buy Now</button></Link> 
                 </div>
               </div>
             </div>
