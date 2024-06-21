b0541efe4b3043484b40f6472b0f8d92d3bb1593
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-06-21 17:50:34 +0600

    Client: Change the name of cart page banner

--- client/src/pages/myCart/MyCart.jsx
+++ client/src/pages/myCart/MyCart.jsx
@@ -36,7 +36,7 @@ const MyCart = () => {
       <div className="hero min-h-[600px] bg-[url('https://themedox.com/mykd/wp-content/themes/mykd/assets/img/bg/breadcrumb_bg02.jpg')] border-b-8 border-main-color">
         <div className="hero-content text-center">
           <div className="max-w-lg">
-            <h1 className="text-6xl font-bold text-white">GAME SINGLE PAGE</h1>
+            <h1 className="text-6xl font-bold text-white">CART PAGE</h1>
           </div>
         </div>
       </div>
