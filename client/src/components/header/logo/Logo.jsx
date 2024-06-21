ac7f0115f8ab89d651e95ce202aaa2581955017d
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-06-21 21:54:45 +0600

    Client: Change the logo

--- client/src/components/header/logo/Logo.jsx
+++ client/src/components/header/logo/Logo.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Logo = () => {
     return (
         <div className="">
-            <Link to={'/'}>  <img className="w-40" src="https://i.ibb.co/qMxM6vQ/logo.png" alt="logo" /></Link>
+            <Link to={'/'}> <h3 className="text-2xl font-bold text-white "><span className="text-7xl">P</span>LAY <span className="text-7xl text-main-color">P</span><span className="text-main-color">LEX</span></h3></Link>
           
         </div>
     );
